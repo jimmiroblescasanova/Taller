d4244749a36b594d501c26f21a1feb5335638fab
Jimmi Robles <jimmirobles@icloud.com>
2024-02-26 13:24:10 -0500

    Added mutator to vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vehicle extends Model
 {
     use HasFactory;
+
+    protected function licensePlate(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => strtoupper($value),
+        );
+    }
 
     public function contact(): BelongsTo
     {
