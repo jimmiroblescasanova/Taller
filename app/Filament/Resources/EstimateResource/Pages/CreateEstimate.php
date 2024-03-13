967de161a17031e6f7658d0c58a4324f91f01af9
Jimmi Robles <jimmirobles@icloud.com>
2024-03-12 20:23:50 -0500

    send email when estimate created

--- app/Filament/Resources/EstimateResource/Pages/CreateEstimate.php
+++ app/Filament/Resources/EstimateResource/Pages/CreateEstimate.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\EstimateResource\Pages;
 
+use App\Mail\EstimateCreated;
+use Illuminate\Support\Facades\Mail;
 use App\Traits\ShouldRedirectToIndex;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Resources\EstimateResource;
@@ -12,5 +14,13 @@ class CreateEstimate extends CreateRecord
     
     protected static string $resource = EstimateResource::class;
 
-    //TODO: send email
+    //TODO: send email to contacto instead admin
+    protected function afterCreate(): void
+    {
+        $estimate = $this->getRecord();
+
+        dispatch(function () use ($estimate) {
+            Mail::to('admin@admin.com')->send(new EstimateCreated($estimate));
+        })->afterResponse();
+    }
 }
